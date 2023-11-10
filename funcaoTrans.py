4097ad34e1845cb996906be203e94ee7d82b0ca2
MatheuAlves <matheu98@hotmail.com>
2023-11-10 10:35:11 -0300

    funcão corrigida

--- funcaoTrans.py
+++ funcaoTrans.py
@@ -1,19 +1,22 @@
-import control as ctrl
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
 # Parâmetros do sistema discreto (você pode ajustar conforme necessário)
-numerator = [1]  # Coeficientes do numerador da função de transferência discreta
-denominator = [1, -0.5]  # Coeficientes do denominador da função de transferência discreta
+numerator = [1.0]  # Coeficientes do numerador da função de transferência discreta
+denominator = [1.0, 1.0, 100.0]  # Coeficientes do denominador da função de transferência discreta
 
 # Tempo de simulação discreto
-time_discrete = np.arange(0, 21, 1)  # Tempo discreto de 0 a 20 com passo 1
+time_discrete = np.arange(0, 20, 1)  # Tempo discreto de 0 a 20 com passo 1
 
 # Criar a função de transferência discreta
-system_discrete = ctrl.TransferFunction(numerator, denominator, dt=1.0)  # dt é o intervalo de amostragem
+system_discrete = signal.dlti(numerator, denominator, dt=1.0)  # dt é o intervalo de amostragem
 
 # Gerar resposta ao degrau discreto
-time_discrete, response_discrete = ctrl.step_response(system_discrete, time_discrete)
+time_discrete, response_discrete = signal.dstep(system_discrete)
+
+# Ajustar o shape da resposta ao degrau
+response_discrete = np.squeeze(response_discrete)
 
 # Plotar a resposta ao degrau discreto
 plt.stem(time_discrete, response_discrete)
